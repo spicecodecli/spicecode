f2de47baca837b2e0c97426fb18310be8083ba42
CodyKoInABox <codykoinabox@gmail.com>
2025-02-25 22:52:51 -0300

    very very very basic cli with typer

--- cli/main.py
+++ cli/main.py
@@ -1,1 +1,22 @@
-print('aaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+
+
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def hello(name: str):
+    print(f"Hello {name}")
+
+
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye{name}")
+    else:
+        print(f"get the fuck out  {name}")
+
+
+if __name__ == "__main__":
+    app()
