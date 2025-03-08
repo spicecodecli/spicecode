512df8996470d4a5c3c8b68c6780eaf5ec101839
CodyKoInABox <codykoinabox@gmail.com>
2025-03-08 15:53:22 -0300

    publish new build to pip (v0.3.8)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt", encoding="utf-16") as f:
 
 setup(
     name="spicecode",
-    version="0.3.6",
+    version="0.3.8",
     packages=find_packages(exclude=["spicecode-venv", "spicecode.egg-info"]),
     install_requires=install_requires,
     entry_points={
