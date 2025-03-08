76ae105eaec1453ecc6c98c419eb44eb480490e9
CodyKoInABox <codykoinabox@gmail.com>
2025-03-08 03:13:33 -0300

    create pt-br translations

---
+++ cli/translations/pt_br.py
@@ -0,0 +1,5 @@
+messages = {
+    "welcome": "🌶️   Bem-vindo ao [bold red]SpiceCode[/]! 🌶️",
+    "description": "🔥 A ferramenta [yellow]CLI[/] que deixa seu código [yellow]mais spicy[/] 🥵",
+    "error": "Erro:",
+}
