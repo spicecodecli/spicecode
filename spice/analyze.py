47e6d3b91513b32317a7b64787399a6ea68e1ceb
CodyKoInABox <codykoinabox@gmail.com>
2025-03-11 21:57:54 -0300

    fix count_comment_lines

--- spice/analyze.py
+++ spice/analyze.py
@@ -139,23 +139,16 @@ def count_functions(ast):
 # this will count comment lines, since our AST/Parser doesn't include comment lines, this needs to be done in the tokenized output of the lexer
 # COMMENT LINE IS A LINE THAT EXCLUSIVELY HAS A COMMENT
 # so like: y = 5 #sets y to 5 IS NOT A COMMENT LINE!!!!!!!!
-def count_comment_lines(tokens):
+def count_comment_lines(code):
+    """Count lines that are exclusively comments (no code on the same line)"""
+    lines = code.split('\n')
     comment_count = 0
-    line_number = 1
-    comment_lines = set()
     
-    for token in tokens:
-        # track line number across all tokens
-        line_number += token.value.count('\n')
-        
-        if token.type == TokenType.COMMENT:
-            # only count if this is the first non-whitespace token on its line
-            # get the line this token starts on
-            token_line = line_number - token.value.count('\n')
-            
-            # check if we already encountered a non-comment token on this line
-            if token_line not in comment_lines:
-                comment_count += 1
-                comment_lines.add(token_line)
+    for line in lines:
+        # remove leading whitespace
+        stripped = line.strip()
+        # check if this line consists only of a comment
+        if stripped and stripped.startswith('#'):
+            comment_count += 1
     
     return comment_count
