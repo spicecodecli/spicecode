47e6deb4e91605bc20f45e7050f70899cf5f20f8
ManfredHair <manfredhair@gmail.com>
2025-03-13 19:29:45 -0300

    separate get_translations() function into own util

--- cli/main.py
+++ cli/main.py
@@ -4,8 +4,9 @@ import importlib
 import typer
 from rich import print
 from InquirerPy import inquirer
-from spice.analyze import analyze_file
 
+from spice.analyze import analyze_file
+from cli.utils.get_translation import get_translation
 
 # initialize typer
 app = typer.Typer()
@@ -19,32 +20,6 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 # select a file to save the current selected langague (if saved to memory it wont persist between commands)
 LANG_FILE = os.path.join(CURRENT_DIR, "lang.txt")
 
-# this will load the translations
-def get_translation():
-    
-    # read the lang file to see what langague was set by user
-    if os.path.exists(LANG_FILE):
-
-        # open the lang file
-        with open(LANG_FILE, "r") as file:
-                
-                # read the lang file
-                lang = file.read().strip()
-
-              
-                if not lang:
-                    lang = 'en' # if file is empty, default to english
-        
-    else:
-        lang = 'en'  # default to English if there is not file but there will always be a file this is just in case ALSO THIS IS SO @icrcode DOESNT COMPLAIN ABOUT MY CODE NOT BEING CLEAN AND WHATEVER
-
-    # this is actually import the translations
-    try:
-        return importlib.import_module(f"cli.translations.{lang}").messages
-    except ModuleNotFoundError:
-        return importlib.import_module("cli.translations.en").messages  # default to English if any errors
-
-    
 
 
 # SPICE SET_LANG COMMAND
@@ -96,7 +71,7 @@ def hello():
     """
 
     # load translations
-    messages = get_translation()
+    messages = get_translation(LANG_FILE)
 
     # print the hello message
     print(messages["welcome"])
@@ -111,7 +86,7 @@ def version():
     """
     
     # load translations
-    messages = get_translation()
+    messages = get_translation(LANG_FILE)
     
     try:
         # Get the path to setup.py in the parent directory
@@ -154,7 +129,7 @@ def analyze(
     """
     
     # load translations
-    messages = get_translation()
+    messages = get_translation(LANG_FILE)
 
     # define available stats UPDATE THIS WHEN NEEDED PLEASE !!!!!!!!
     available_stats = [
