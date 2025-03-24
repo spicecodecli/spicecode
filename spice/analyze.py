7cb7546fb1135a9a3cf11d21a5ce93116add7ff4
CodyKoInABox <codykoinabox@gmail.com>
2025-03-24 12:06:28 -0300

    separate count functions analyzer into own file

--- spice/analyze.py
+++ spice/analyze.py
@@ -6,11 +6,12 @@ from spice.utils.get_lexer import get_lexer_for_file
 # import analyzer functions from analyzers folder
 from spice.analyzers.count_lines import count_lines
 from spice.analyzers.count_comment_lines import count_comment_lines
+from spice.analyzers.count_functions import count_functions
 
 # gustavo testando alguma coisa 
 from spice.analyzers.identation import detect_indentation
 
-    
+
 
 # this is the analyze function
 def analyze_file(file_path: str, selected_stats=None):
@@ -71,53 +72,6 @@ def analyze_file(file_path: str, selected_stats=None):
     
     return results
 
-
-
-# this will count functions in the AST
-def count_functions(ast):
-    # import function definition from the parser's ast
-    from parser.ast import FunctionDefinition, Program
-    
-    if not isinstance(ast, Program):
-        return 0
-    
-    function_count = 0
-    
-    # recursive search for function definitions in the AST
-    def search_node(node):
-        nonlocal function_count
-        
-        if isinstance(node, FunctionDefinition):
-            function_count += 1
-        
-        # process child nodes if they exist
-        if hasattr(node, 'statements') and node.statements:
-            for statement in node.statements:
-                search_node(statement)
-        
-        if hasattr(node, 'body') and node.body:
-            for body_statement in node.body:
-                search_node(body_statement)
-        
-        # for binary operation, check both sides
-        if hasattr(node, 'left'):
-            search_node(node.left)
-        if hasattr(node, 'right'):
-            search_node(node.right)
-        
-        # check the value part of an assignment
-        if hasattr(node, 'value'):
-            search_node(node.value)
-            
-        # check function call arguments
-        if hasattr(node, 'arguments') and node.arguments:
-            for arg in node.arguments:
-                search_node(arg)
-    
-    # start recursive search from the root Program node
-    search_node(ast)
-    
-    return function_count
 
 
 def analyze_code_structure(code):
