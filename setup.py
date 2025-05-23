35cae2dcc04cfae3c5a1f88f49c3361634072c92
CodyKoInABox <codykoinabox@gmail.com>
2025-05-23 18:40:14 -0300

    update pypi info

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ from setuptools import setup, find_packages
 with open("requirements.txt", encoding="utf-16") as f:
     install_requires = f.read().splitlines()
 
+# Read the README.md for PyPI long description
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="spicecode",
     version="2.1.5", # version 2.0.0 = all features from N2 
@@ -14,5 +18,16 @@ setup(
             'spice = cli.main:main',  # This tells Python to run the main function in cli.main
         ],
     },
+    description="SpiceCode: The next generation of code analysis.",  # Short one-liner
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Required if README is in Markdown
+    author="SpiceCodeCLI",
+    author_email="spicecodecli@gmail.com",
+    url="https://github.com/spicecodecli/spicecode",  # Optional but helpful
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Apache License",  # Adjust if needed
+        "Operating System :: Windows :: Linux :: Possibly MAC, not tested.",
+    ],
 
 )
