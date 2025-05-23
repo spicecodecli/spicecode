3a41084296bbc9c76dd3203d7db90a5aa050a314
ManfredHair <manfredhair@gmail.com>
2025-05-23 20:18:36 -0300

    activate sphinx autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,11 @@ author = 'SpiceCodeCLI'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
+
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
