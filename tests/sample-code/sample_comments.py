0b1c9c9ad7034cbee5d1c6090c43a654b330a9dc
CodyKoInABox <codykoinabox@gmail.com>
2025-05-24 03:22:01 -0300

    python comments tests sample code

---
+++ tests/sample-code/sample_comments.py
@@ -0,0 +1,15 @@
+# This is a full comment line
+import os # This is an inline comment
+
+# Another full comment line
+
+def func():
+    pass # Inline comment on pass
+
+#
+# Multi-line style comment (still single lines)
+#
+"""
+This is a docstring, not a comment line.
+"""
+
