8c4adee3210bf0d4a20d9a50b6ea6a9942462284
CodyKoInABox <codykoinabox@gmail.com>
2025-05-24 03:21:20 -0300

    go code radio tests sample code

---
+++ tests/sample-code/ratio_sample.go
@@ -0,0 +1,18 @@
+// Go sample for comment ratio
+// Full comment line 1
+
+package main // Inline comment
+
+import "fmt"
+
+/*
+ * Multi-line comment block
+ * Line 2
+ */
+
+// Full comment line 2
+
+func main() { // Another inline
+	fmt.Println("Hello")
+}
+
