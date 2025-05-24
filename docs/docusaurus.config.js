a610a6e5a89ee9bdfcebf3dd966e46e630e7f446
CodyKoInABox <codykoinabox@gmail.com>
2025-05-24 02:16:13 -0300

    add social card

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card // what even is a social card?
-      image: 'img/spicecode-social-card.jpg', // Placeholder social card
+      image: 'img/spicecode_wallpaper.png', // Placeholder social card
       navbar: {
         title: 'SpiceCode',
         logo: {
