025bf1ebb0bcfc5ecdbf5305ed2d365cf85cfe0f
CodyKoInABox <codykoinabox@gmail.com>
2025-06-30 12:35:27 -0300

    new types for new metrics

--- spicecloud/pages/utils/types.ts
+++ spicecloud/pages/utils/types.ts
@@ -4,7 +4,29 @@ export interface MetricData {
   timestamp: number;
   file_name: string;
   file_path: string;
-  metrics: any;
+  metrics: {
+    file_extension: string;
+    line_count: number;
+    comment_line_count: number;
+    inline_comment_count: number;
+    indentation_type: string;
+    indentation_size: number;
+    function_count: number;
+    external_dependencies_count: number;
+    method_type_count: { private: number; public: number };
+    comment_ratio: string;
+    average_function_size?: number;
+
+    /* duplicate code */
+    duplicate_blocks?: number;
+    duplicate_lines?: number;
+    duplicate_percentage?: number;
+
+    /* complexity */
+    average_complexity?: string;
+    complexity_distribution?: Record<string, number>;
+    total_analyzed_functions?: number;
+  };
   age: number;
   readable_timestamp: string;
 }
