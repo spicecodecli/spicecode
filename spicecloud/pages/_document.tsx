525cf848459caa3e45e98bea06eff972e5d7d05b
CodyKoInABox <codykoinabox@gmail.com>
2025-06-30 01:39:24 -0300

    remove title from _document  to avoid warning

--- spicecloud/pages/_document.tsx
+++ spicecloud/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>SpiceCloud | Powered by SpiceCodeCLI</title>
         <link rel="icon" href="/spicecode-logo.png" />
       </Head>
       <body className="antialiased">
